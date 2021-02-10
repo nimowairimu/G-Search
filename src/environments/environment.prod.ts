cfaf7366ea0072b986530db7cda0233229901c4e
nimowairimu <nimowairimu25@gmail.com>
2021-02-10 12:47:50 +0300

    Change apiKey.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiKey: "8ae37c48f07fbf969406f68d63b0ef3f28c38b53"
+  apiKey: "7759bb83a56956b485fc3c113665641770c347e9"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-apiKey:"8ae37c48f07fbf969406f68d63b0ef3f28c38b53"
+apiKey:"7759bb83a56956b485fc3c113665641770c347e9"
 };
 
 /*
